9ebc32de60a3bc58d295e415645cb6ee5f3be277
luiggy macias <ferrinluiggy@gmail.com>
2022-01-19 22:37:31 -0500

    first commit

---
+++ ejercicio1.php
@@ -0,0 +1,10 @@
+<?php 
+// Introduccion de impresión the información
+echo 'Hola luiggy ';
+/*
+Mensaje largo
+Este es un comentario de codigo
+*/
+print_r('hola luiggy from print_r');
+print('hola luiggy from print');
+?>
